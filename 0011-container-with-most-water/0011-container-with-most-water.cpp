aba044aadaeb2dce09c62f877ee2d97aa9bc5f51
Satyam Patil <157580492+Satyam-Private@users.noreply.github.com>
2026-08-07 22:24:26 +0530

    Time: 0 ms (100%), Space: 62.8 MB (98.36%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        int start = 0, end = height.size() - 1; 
-        cout<<start<<end;
-        int maxCap = INT_MIN;
-        while(start <= end){
-            int holdingCap = min(height[start] , height[end]) * (end - start);
+        
+        int size = height.size(); 
 
-            if(height[start] <= height[end]){
-                start++; 
+        int start = 0, end = size-1;
+        int maxWater = INT_MIN; 
+        while(start <= end){
+            int water = min(height[start] , height[end])*(end-start);
+            maxWater = max(maxWater , water);
+            if(height[start] < height[end]){
+                start++;
             }
             else{ 
                 end--; 
             }
-
-            maxCap = max(maxCap , holdingCap); 
         }
-
-        return maxCap; 
+        return maxWater; 
     }
 };
