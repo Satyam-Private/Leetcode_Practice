bbcf334a743f06ae57fbe9dfe3a53dd95db788bd
Satyam Patil <157580492+Satyam-Private@users.noreply.github.com>
2026-09-25 10:24:37 +0530

    Time: 53 ms (88.31%), Space: 149.3 MB (83.44%) - LeetHub

--- 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -1,37 +1,29 @@
 class Solution {
 public:
     int maxSubarrayLength(vector<int>& nums, int k) {
-        if(nums.size() <= k){
-            return nums.size(); 
-        }
-        
-        int start = 0, end = 0; 
-        unordered_map<int,int> freqMap; 
 
-        int size = 0, maxSize = INT_MIN; 
-        while(end < nums.size()){
-            if(freqMap.find(nums[end]) == freqMap.end()){
-                freqMap[nums[end]]++;
-                end++;  
+        unordered_map<int, int> freqMap;
+
+        int left = 0;
+        int ans = 0;
+
+        for (int right = 0; right < nums.size(); right++) {
+
+            // 1. Add current element
+            freqMap[nums[right]]++;
+
+            // 2. If frequency exceeds k, shrink window
+            while (freqMap[nums[right]] > k) {
+                freqMap[nums[left]]--;
+                left++;
             }
 
-            else{
-                if(freqMap[nums[end]] < k){
-                    freqMap[nums[end]]++; 
-                    end++;
-                }
-                else if(freqMap[nums[end]] >= k){ 
-                    
-                    size = end - start; 
-                    maxSize = max(size , maxSize) ;
-                    freqMap[nums[start]]--; 
-                    start++; 
-                                    }
-            }
+            // 3. Calculate current valid window
+            int size = right - left + 1;
 
-              size = end - start; 
-                    maxSize = max(size , maxSize) ;
+            ans = max(ans, size);
         }
-        return maxSize; 
+
+        return ans;
     }
 };
